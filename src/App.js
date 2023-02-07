a126e7958730f1c4b8a3d2ce4b3a3d0d91e447d8
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-07 17:27:07 +0700

    adding react icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import "./App.scss";
+import { FaBehance, FaDribbble } from "react-icons/fa";
+import {
+  IoMailOpenOutline,
+  IoChevronForwardCircle,
+  IoStar,
+} from "react-icons/io5";
+import { IconContext } from "react-icons";
 
 function App() {
   return (
@@ -8,7 +15,22 @@ function App() {
           daily<span>frontend</span>
         </div>
         <div className="menu_container">
-          <span></span>
+          <span>
+            <IconContext.Provider
+              value={{
+                color: "#000",
+                size: "18px",
+                className: "icons_container",
+              }}
+            >
+              <div className="icon">
+                <FaBehance />
+              </div>
+              <div className="icon">
+                <FaDribbble />
+              </div>
+            </IconContext.Provider>
+          </span>
         </div>
       </header>
     </>
